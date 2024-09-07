9f69a82b101e38e9be15902c855e5e4980889f70
JuanCardonaB <juanjocarbol@gmail.com>
2024-09-07 15:16:06 -0500

    I just add the structure of the introduction

---
+++ src/components/Intro.tsx
@@ -0,0 +1,15 @@
+// const styles = {
+//     image: '',
+// }
+
+export const Intro = () => {
+  return (
+    <div className="flex w-full flex-col">
+        <h1 className="text-5xl">Juan Jose</h1>
+        <h1>Cardona Bolivar</h1>
+        <p>Introduction</p>
+        <img className='max-w-lg' src="https://img.freepik.com/foto-gratis/cierre-dientes-hombre-moreno-sonriendo_1187-5800.jpg?w=826&t=st=1725737542~exp=1725738142~hmac=9f8d8d2a9f7d309391e0c283de20d0e5b50561547634814af3ed9fae3f8c468c" alt="" />
+    </div>
+
+  )
+}
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,7 +1,11 @@
+import { Intro } from "../components/Intro"
+
 export const Home = () => {
   return (
-    <p>
-        Home
-    </p>
+    <div className="flex justify-center">
+      <div className="w-full flex flex-col max-w-[1024px] px-6">
+        <Intro />
+      </div>
+    </div>
   )
 }
