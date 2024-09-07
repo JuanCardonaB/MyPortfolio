a9b75b429b09bb1d0d24d940ab13b011328a0413
JuanCardonaB <juanjocarbol@gmail.com>
2024-09-07 17:36:39 -0500

    Resolve conversation

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -6,7 +6,9 @@ export const Intro = () => {
     const {scrollValue} = useContext(ScrollContext)
     const [opacity, setOpacity] = useState(0)
 
+    // each time the scrollValue changes, the opacity is updated
     useEffect(() => {
+        // opacity has a value between 0 and 1, so we divide the scrollValue by 100
         setOpacity(Number((scrollValue / 100).toFixed(1)))
     }, [scrollValue])
 
@@ -14,7 +16,7 @@ export const Intro = () => {
         <div style={{ marginRight: `${scrollValue}px` }} className="flex w-full flex-col items-start sm:items-center">
             <GoogleFontLoader fonts={[{ font: 'Shrikhand', weights: [400, 700] }, {font: 'Jersey 15', weights: [400]}]} />
 
-            <h1 style={{ fontFamily: 'Shrikhand, sans-serif', marginRight: `${scrollValue}px`, marginTop: 100 }} className="text-[40px] sm:text-[60px] text-violet-800">Hi I'm</h1>
+            <h1 style={{ fontFamily: 'Shrikhand, sans-serif', marginRight: `${scrollValue}px`, marginTop: 150 }} className="text-[40px] sm:text-[60px] text-violet-800">Hi I'm</h1>
             <h1 style={{ fontFamily: 'Shrikhand, sans-serif', marginRight: `${scrollValue / 2}px` }} className="text-[60px] mt-[-40px] sm:text-[100px] md:text-[100px]">Juan Jose</h1>
             <h1 style={{ fontFamily: 'Shrikhand, sans-serif', marginRight: `${scrollValue / 2}px` }} className="text-[60px] mt-[-50px] sm:text-[100px] md:text-[100px] tracking-wider">Cardona</h1>
             <p style={{ fontFamily: 'Jersey 15, sans-seif', opacity: opacity }}>Full Stack Software Developer</p>
--- src/contexts/ScrollContext.tsx
+++ src/contexts/ScrollContext.tsx
@@ -1,18 +1,22 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 
+// We create the context with the default values, and their types
 interface ScrollContextProps {
     scrollValue: number;
     setScrollValue: (value: number) => void;
 }
 
+// We create the context with the default values, and their types
 export const ScrollContext = createContext({} as ScrollContextProps);
 
+// We create the provider that will be used in the main file
 export const ScrollProvider = ({ children }: { children: ReactNode}) => {
     const [scrollValue, setScrollValue] = useState(0)
 
+    // We create a listener that will update the scrollValue each time the user scrolls
     useEffect(() => {
         const handleScroll = () => {
-            setScrollValue(window.scrollY) // Ajusta el divisor para cambiar la sensibilidad
+            setScrollValue(window.scrollY)
         }
         window.addEventListener('scroll', handleScroll)
         return () => {
