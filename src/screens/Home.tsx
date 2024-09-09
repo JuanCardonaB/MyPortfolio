79b641e39bd75fbdacba054437a20966b3eefb2b
JuanCardonaB <juanjocarbol@gmail.com>
2024-09-08 20:25:14 -0500

    (Done): Create the bio section.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,14 @@
+import { AboutMe } from "../components/AboutMe"
 import { Intro } from "../components/Intro"
+// import { WorkExperience } from "../components/WorkExperience"
 
 export const Home = () => {
   return (
     <div className="flex justify-center h-[4000px]">
-      <div className="w-full flex flex-col max-w-[1024px] px-6">
+      <div className="w-full flex flex-col px-6">
         <Intro />
+        <AboutMe />
+        {/* <WorkExperience /> */}
       </div>
     </div>
   )
