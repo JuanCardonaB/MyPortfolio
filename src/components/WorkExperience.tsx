07d670abe696a3a231b9c6019e22e280e7991894
JuanCardonaB <juanjocarbol@gmail.com>
2024-09-09 22:52:18 -0500

    Made a component for work experiences

--- src/components/WorkExperience.tsx
+++ src/components/WorkExperience.tsx
@@ -1,5 +1,5 @@
-import { Code } from "@nextui-org/react"
 import GoogleFontLoader from "react-google-font-loader"
+import { WorkExperienceCard } from "./WorkExperienceCard"
 
 export const WorkExperience = () => {
   return (
@@ -9,7 +9,7 @@ export const WorkExperience = () => {
       <div className="max-w-[1024px] w-full">
         <div className="flex justify-center items-center gap-3">
           <h2 className="text-[50px]" style={{ fontFamily: 'Shrikhand, sans-serif' }} >Work</h2>
-          <h2 className="text-[50px] text-violet-800" style={{ fontFamily: 'Shrikhand, sans-serif' }}>Experience</h2>
+          <h2 className="text-[50px] text-violet-800" style={{ fontFamily: 'Shrikhand, sans-serif' }}>Experience 💼</h2>
         </div>
         <div className="flex justify-center w-full mt-10 gap-10">
           <div className="h-[650px] w-1 bg-violet-800 rounded-lg">
@@ -19,68 +19,9 @@ export const WorkExperience = () => {
           </div>
 
           <div className="pt-8 flex flex-col gap-16">
-            <div style={{ transitionDelay: '100ms' }} className="flex gap-6 w-fit h-[200px] rounded-lg transition-all duration-500 m-[-16px] hover:m-4">
-
-              <div className="w-[250px]">
-                <h3 className="text-[25px]">INFERA</h3>
-                <p>Full Stack Mobile Developer</p>
-                <p>2024 - Present</p>
-                <div className="flex flex-wrap gap-1 mt-4">
-                  <Code color="secondary">PHP</Code>
-                  <Code color="secondary">React Native</Code>
-                  <Code color="secondary">SQL</Code>
-                </div>
-              </div>
-
-              <div className="w-[400px]">
-                <p>I'm working on INFERA, making new features that the app require</p>
-
-                <p></p>
-              </div>
-
-            </div>
-
-            <div style={{ transitionDelay: '100ms' }} className="flex gap-6 w-fit h-[200px] rounded-lg transition-all duration-500 m-[-16px] hover:m-4">
-
-              <div className="w-[250px]">
-                <h3 className="text-[25px]">INFERA</h3>
-                <p>Full Stack Web Developer</p>
-                <p>2024 - 2024</p>
-                <div className="flex flex-wrap gap-1 mt-4">
-                  <Code color="secondary">Python</Code>
-                  <Code color="secondary">React</Code>
-                  <Code color="secondary">Fast API</Code>
-                </div>
-              </div>
-
-              <div className="w-[400px]">
-                <p>I'm working on INFERA, making new features that the app require</p>
-
-                <p></p>
-              </div>
-
-            </div>
-
-            <div style={{ transitionDelay: '100ms' }} className="flex gap-6 w-fit h-[200px] rounded-lg transition-all duration-500 m-[-16px] hover:m-4">
-
-              <div className="w-[250px]">
-                <h3 className="text-[25px]">INFERA</h3>
-                <p>Full Stack Mobile Developer</p>
-                <p>2024 - Present</p>
-                <div className="flex flex-wrap gap-1 mt-4">
-                  <Code color="secondary">PHP</Code>
-                  <Code color="secondary">React Native</Code>
-                  <Code color="secondary">SQL</Code>
-                </div>
-              </div>
-
-              <div className="w-[400px]">
-                <p>I'm working on INFERA, making new features that the app require</p>
-
-                <p></p>
-              </div>
-
-            </div>
+            <WorkExperienceCard jobTitle="INFERA" rol="Full Stack Mobile Developer" technologies={['PHP','React Native','SQL']} time="2024 - present" />
+            <WorkExperienceCard jobTitle="BP Med" rol="Full Stack Web Developer" technologies={['Python','React','Fast API']} time="2024 - 2024" />
+            <WorkExperienceCard jobTitle="INFERA" rol="Full Stack Mobile Developer" technologies={['PHP','React Native','SQL']} time="2024 - present" />
           </div>
         </div>
       </div>
