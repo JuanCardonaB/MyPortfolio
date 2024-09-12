37e30563aefa894ed5df3f8ba30257a127e57854
JuanCardonaB <juanjocarbol@gmail.com>
2024-09-12 16:43:59 -0500

    Make sure that work experience and about me sections are translated and responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,14 @@ module.exports = {
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'custom-sm': '715px',
+        'custom-md': '875px',
+        'custom-lg': '1200px',
+
+      }
+    },
   },
   darkMode: 'class', // o 'media' o 'class'
   plugins: [
