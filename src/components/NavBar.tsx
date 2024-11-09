a834d9fb5763b90a793eabc697482e6c9822847b
JuanCardonaB <juanjocarbol@gmail.com>
2024-11-08 22:35:58 -0500

    MyPortfolio-8

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,26 +3,38 @@ import { ThemeSwitcher } from "./ThemeSwitcher"
 import { useTranslation } from 'react-i18next'
 import { LanguageSwitcher } from "./LanguageSwitcher"
 
-export const NavBar = () => {
+interface NavBarProps {
+  aboutRef: React.RefObject<HTMLDivElement>;
+  experienceRef: React.RefObject<HTMLDivElement>;
+  projectsRef: React.RefObject<HTMLDivElement>;
+  intro: React.RefObject<HTMLDivElement>;
+}
+
+export const NavBar = ({ intro, aboutRef, experienceRef, projectsRef }: NavBarProps) => {
   const { t } = useTranslation('global')
+
+  const scrollTo = (ref: React.RefObject<HTMLDivElement>) => {
+    ref.current?.scrollIntoView({ behavior: 'smooth' })
+  }
+
   return (
     <Navbar shouldHideOnScroll>
-      <NavbarBrand>
+      <NavbarBrand className="cursor-pointer" onClick={() => scrollTo(intro)}>
         {t('Navbar.Home')}
       </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
       <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link className="cursor-pointer" color="foreground" onClick={() => scrollTo(aboutRef)}>
             {t('Navbar.About')}
           </Link>
         </NavbarItem>
         <NavbarItem isActive>
-          <Link href="#" aria-current="page">
+          <Link className="cursor-pointer" onClick={() => scrollTo(experienceRef)} aria-current="page">
             {t('Navbar.Experience')}
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="#">
+          <Link className="cursor-pointer" color="foreground" onClick={() => scrollTo(projectsRef)}>
             {t('Navbar.Projects')}
           </Link>
         </NavbarItem>
